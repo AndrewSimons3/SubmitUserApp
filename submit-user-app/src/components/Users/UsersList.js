0f4a21d5f5b60e9d415ae8e765edaef655355925
AndrewSimons3 <simdrew07@gmail.com>
2021-04-22 07:59:53 -0500

    users list now rendering

--- submit-user-app/src/components/Users/UsersList.js
+++ submit-user-app/src/components/Users/UsersList.js
@@ -3,15 +3,17 @@ import Card from '../UI/Card';
 import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
-  <Card className={classes.users}> 
-    <ul>
-      {props.users.map((user) => (
-        <li>
-          {user.name} ({user.age} years old)
-        </li>
-      ))}
-    </ul>
-  </Card>
+  return (
+    <Card className={classes.users}> 
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  )
 };
 
 export default UsersList;
