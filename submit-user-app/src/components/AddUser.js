032199cf81d4a2cde3de52c771f3355019962719
AndrewSimons3 <simdrew07@gmail.com>
2021-04-22 07:18:58 -0500

    removed inputs from ui when adding user and filtered valid requirements for input

--- submit-user-app/src/components/AddUser.js
+++ submit-user-app/src/components/AddUser.js
@@ -1,24 +1,48 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
 
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
 
   const addUserHandler = (event) => {
     event.preventDefault();
     
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername);
+    console.log(enteredAge)
+ 
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
   }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  }
+
   
   return (
-    <div className="user">
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <input id="username" type="text" value={enteredUsername} onChange={userNameChangeHandler}></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" value ={enteredAge} onChange={ageChangeHandler}></input>
+        <Button type="submit">Add User</Button>
       </form>
-    </div>
+    </Card>
      
   )
 }
