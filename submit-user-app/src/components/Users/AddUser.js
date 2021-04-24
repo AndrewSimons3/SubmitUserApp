968fba8829bb5812d1f80c24b59065a60971c24a
AndrewSimons3 <simdrew07@gmail.com>
2021-04-24 10:24:00 -0500

    restructured input elements

--- submit-user-app/src/components/Users/AddUser.js
+++ submit-user-app/src/components/Users/AddUser.js
@@ -37,9 +37,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" value={enteredUsername} onChange={userNameChangeHandler}></input>
+        <input 
+          id="username" 
+          type="text" 
+          value={enteredUsername} 
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" value ={enteredAge} onChange={ageChangeHandler}></input>
+        <input 
+          id="age" 
+          type="number" 
+          value ={enteredAge} 
+          onChange={ageChangeHandler}  
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
