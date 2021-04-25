bb9316be10ac321c25b385c53a36a17487b90960
AndrewSimons3 <simdrew07@gmail.com>
2021-04-25 16:04:50 -0500

    button component created

---
+++ submit-user-app/src/components/UI/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const Button = (props) => {
+
+  const addUserHandler = (event) => {
+    console.log(event.target.value)
+  }
+
+  return (
+    <button
+      className={classes.button}
+      type={props.type || 'button'}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  )
+};
+
+export default Button;
